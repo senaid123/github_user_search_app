65babc791e845606b3f8d5230bf40722299577ec
senaid123 <senaid968@gmail.com>
2022-10-13 17:40:48 +0200

    Search implementation, reorganize code

--- lib/github_user_search_app_web/live/github_user_live/index.ex
+++ lib/github_user_search_app_web/live/github_user_live/index.ex
@@ -1,20 +1,46 @@
 defmodule GithubUserSearchAppWeb.GithubUserLive.Index do
   use GithubUserSearchAppWeb, :live_view
+  use Phoenix.HTML
   alias GithubUserSearchApp.UserGetter
+  alias GithubUserSearchAppWeb.GithubUserLive.Icons
 
   def mount(_params, _session, socket) do
-    {:ok, user} = UserGetter.get_github_user("senaid123")
-
     socket =
-      socket
-      |> assign(:github_user, user)
+      assign(socket,
+        github_user: nil,
+        search: "",
+        loading: false
+      )
 
     {:ok, socket}
   end
 
-  def handle_event("search", %{"user" => user} = _params, socket) do
-    UserGetter.get_github_user(user)
-    socket = assign(socket, :user, user)
+  def handle_event("search", %{"search" => search}, socket) do
+    send(self(), {:run_search, search})
+
+    socket =
+      assign(socket,
+        github_user: nil,
+        search: "",
+        loading: true
+      )
+
     {:noreply, socket}
+  end
+
+  def handle_info({:run_search, search}, socket) do
+    case UserGetter.get_github_user(search) do
+      {:ok, github_user} ->
+        socket = assign(socket, github_user: github_user, loading: false)
+        {:noreply, socket}
+
+      _ ->
+        socket =
+          socket
+          |> put_flash(:info, "No users matching \"#{search}\"")
+          |> assign(gitub_user: "", loading: false)
+
+        {:noreply, socket}
+    end
   end
 end
