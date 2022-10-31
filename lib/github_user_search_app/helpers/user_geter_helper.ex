74a98d84790ad09a6d07bd943a56c7cb096bb180
senaid123 <senaid968@gmail.com>
2022-10-31 17:38:14 +0100

    Remove headers because they are unnecessary

--- lib/github_user_search_app/helpers/user_geter_helper.ex
+++ lib/github_user_search_app/helpers/user_geter_helper.ex
@@ -4,15 +4,7 @@ defmodule GithubUserSearchApp.Helpers.UserGeterHelper do
   """
 
   def get_user(data) do
-    Finch.build(:get, "https://api.github.com/users/" <> data, get_headers())
+    Finch.build(:get, "https://api.github.com/users/" <> data)
     |> Finch.request(MyFinch)
-  end
-
-  defp get_headers do
-    token = System.get_env("TOKEN")
-
-    headers = [{"Authorization", token}]
-
-    headers
   end
 end
